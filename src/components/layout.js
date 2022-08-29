76bb050318c09357e2d16c4987c0fdcfb159a540
vincentjr <contact@vincentjr.com>
2022-08-29 15:25:39 -0400

    adding tw script

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,6 +28,18 @@ const SiteLayout = ({ children }) => (
 	  		© {new Date().getFullYear()}; Last updated Aug. 2020
 		  </footer>
     </main>
+    <Helmet>
+    <script>
+      {`
+        {
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+          twq('config','obv91');
+        }
+      `}
+      </script>
+    </Helmet>
   </div>
 )
 
